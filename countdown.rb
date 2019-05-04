f1aedb4e381a4417d5d92e4c0b968f2ee6b50722
Irma J Mejia <ijanettmejia@outlook.com>
2019-05-04 00:16:00 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -8,3 +8,13 @@ def countdown(integer)
   end
   puts "HAPPY NEW YEAR!"
 end
+
+def countdown_with_sleep(integer)
+  integer = 10
+  while integer >= 0
+    puts "#{integer} SECONDS!"
+    sleep 1
+    integer -= 1
+  end
+  "HAPPY NEW YEAR!"
+end
